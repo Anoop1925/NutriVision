d2f6bb9c0eb32fed277cbb782c81c23d840ed870
Anoop1619 <panoop2005.ap@gmail.com>
2026-02-26 18:19:11 +0530

    Railway deploy: Procfile, requirements, PORT binding, chart fixes

--- app/app.py
+++ app/app.py
@@ -35,4 +35,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5000, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=False, port=port, use_reloader=False)
